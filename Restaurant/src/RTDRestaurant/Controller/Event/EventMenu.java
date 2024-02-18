a264380fcc44196a7c4c96243b7443b57f97998b
nnphuonguyen <133204633+nnphuonguyen@users.noreply.github.com>
2024-02-18 21:31:16 +0700

    Delete Restaurant directory

--- Restaurant/src/RTDRestaurant/Controller/Event/EventMenu.java
+++
@@ -1,8 +0,0 @@
-package RTDRestaurant.Controller.Event;
-
-import java.awt.Component;
-
-//Event Menu Press
-public interface EventMenu {
-    public boolean menuPress(Component com,boolean open);
-}
--- Restaurant/src/RTDRestaurant/Controller/Event/EventMenuSelected.java
+++
@@ -1,7 +0,0 @@
-
-package RTDRestaurant.Controller.Event;
-
-//Event khi chọn menu (menu index + subMenu index)
-public interface EventMenuSelected {
-    public void menuSelected(int menuIndex,int subMenuIndex);
-}
